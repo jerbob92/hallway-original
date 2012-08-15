2f15f998147f8c4c8b3ff4f4d96d9e0cdaf70e61
Beau Gunderson <beau@beaugunderson.com>
2012-08-15 16:27:24 -0700

    Cleanup for hallwayd.js, defaults.json

--- hallwayd.js
+++ hallwayd.js
@@ -1,11 +1,11 @@
 /*
-*
-* Copyright (C) 2011, The Locker Project
-* All rights reserved.
-*
-* Please see the LICENSE file for more information.
-*
-*/
+ *
+ * Copyright (C) 2011, The Locker Project
+ * All rights reserved.
+ *
+ * Please see the LICENSE file for more information.
+ *
+ */
 
 exports.alive = false;
 
@@ -13,45 +13,51 @@ var fs = require('fs');
 var path = require('path');
 var async = require('async');
 var util = require('util');
-var argv = require("optimist").argv;
+var argv = require('optimist').argv;
 
 var Roles = {
   worker: {
-    startup:startWorkerWS
+    startup: startWorkerWS
   },
   apihost: {
-    startup:startAPIHost
+    startup: startAPIHost
   },
   dawg: {
-    startup:startDawg
+    startup: startDawg
   },
   stream: {
-    startup:startStream
+    startup: startStream
   }
 };
+
 var role = Roles.apihost;
 
-// This lconfig stuff has to come before any other hallway modules are loaded!
+// lconfig has to be loaded before any other hallway modules!
 var lconfig = require('lconfig');
 var configDir = process.env.LOCKER_CONFIG || 'Config';
+
 if (!lconfig.loaded) {
-    var configFile;
-    if (process.argv[2] === '--config') {
-        configFile = process.argv[3];
-    }
-    else {
-        configFile = path.join(configDir, 'config.json');
-    }
-    lconfig.load(configFile);
-}
-else {
-    console.warn("Hallway config already loaded");
+  var configFile;
+
+  if (process.argv[2] === '--config') {
+    configFile = process.argv[3];
+  } else {
+    configFile = path.join(configDir, 'config.json');
+  }
+
+  lconfig.load(configFile);
+} else {
+  console.warn("Hallway config already loaded");
 }
 
-var logger = require("logger").logger("hallwayd");
+var logger = require('logger').logger('hallwayd');
+
 logger.info('process id:' + process.pid);
+
 var alerting = require("alerting");
-lconfig.alerting = {key:1}
+
+lconfig.alerting = { key: 1 };
+
 if (lconfig.alerting && lconfig.alerting.key) {
   alerting.init(lconfig.alerting);
   alerting.install(function(E) {
@@ -59,11 +65,13 @@ if (lconfig.alerting && lconfig.alerting.key) {
     shutdown(1);
   });
 }
-var taskman = require("taskman");
+var taskman = require('taskman');
 var pipeline = require('pipeline');
 var profileManager = require('profileManager');
+
+var http = require('http');
+
 // Set our globalAgent sockets higher
-var http = require("http");
 http.globalAgent.maxSockets = 800;
 
 var shuttingDown_ = false;
@@ -76,9 +84,12 @@ function startTaskman(cbDone) {
 
 function startAPIHost(cbDone) {
   logger.info("Starting an API host");
+
   var webservice = require('webservice');
+
   webservice.startService(lconfig.lockerPort, lconfig.lockerListenIP, function(hallway) {
     logger.info('Hallway is now listening at ' + lconfig.lockerBase);
+
     cbDone();
   });
 }
@@ -86,21 +97,27 @@ function startAPIHost(cbDone) {
 function startDawg(cbDone) {
   if (!lconfig.dawg || !lconfig.dawg.port || !lconfig.dawg.password) {
     logger.error("You must specify a dawg section with at least a port and password to run.");
+
     shutdown(1);
   }
+
   logger.info("Starting a Hallway Dawg -- Think you can get away without having a hall pass?  Think again.");
-  var dawg = require("dawg");
-  if (!lconfig.dawg.listenIP) lconfig.dawg.listenIP = "0.0.0.0";
+
+  var dawg = require('dawg');
+
   dawg.startService(lconfig.dawg.port, lconfig.dawg.listenIP, function() {
     logger.info("The Dawg is now monitoring at port %d", lconfig.dawg.port);
+
     cbDone();
   });
 }
 
 function startStream(cbDone) {
   logger.info("Starting a Hallway Stream -- you're in for a good time.");
-  require("streamer").startService(lconfig.stream, function() {
+
+  require('streamer').startService(lconfig.stream, function() {
     logger.info("Streaming at port %d", lconfig.stream.port);
+
     cbDone();
   });
 }
@@ -121,8 +138,10 @@ function startWorkerWS(cbDone) {
 if (argv._.length > 0) {
   if (!Roles.hasOwnProperty(argv._[0])) {
     logger.error("The %s role is unknown.", argv._[0]);
+
     return shutdown(1);
   }
+
   role = Roles[argv._[0]];
 }
 
@@ -146,49 +165,56 @@ if (role.startup) {
 async.series(startupTasks, function(error) {
   // TODO:  This needs a cleanup, it's too async
   logger.info("Hallway is up and running.");
+
   exports.alive = true;
 });
 
 // scheduling and misc things
 function shutdown(returnCode, callback) {
-    if (shuttingDown_ && returnCode !== 0) {
-        try {
-            console.error("Aieee! Shutdown called while already shutting down! Panicking!");
-        }
-        catch (e) {
-            // we tried...
-        }
-        process.exit(1);
+  if (shuttingDown_ && returnCode !== 0) {
+    try {
+      console.error("Aieee! Shutdown called while already shutting down! Panicking!");
     }
-    shuttingDown_ = true;
-    process.stdout.write("\n");
-    logger.info("Shutting down...");
-    if (callback) {
-      return callback(returnCode);
+    catch (e) {
+      // we tried...
     }
-    else {
-      return exit(returnCode);
-    }
+
+    process.exit(1);
+  }
+
+  shuttingDown_ = true;
+  process.stdout.write("\n");
+  logger.info("Shutting down...");
+
+  if (callback) {
+    return callback(returnCode);
+  }
+
+  exit(returnCode);
 }
 
 function exit(returnCode) {
   logger.info("Shutdown complete");
+
   process.exit(returnCode);
 }
 
 process.on("SIGINT", function() {
   logger.info("Shutting down via SIGINT...");
+
   switch (role) {
-  case Roles.worker:
-    taskman.stop(function() {
+    case Roles.worker:
+      taskman.stop(function() {
+        shutdown(0);
+      });
+      break;
+    case Roles.apihost:
       shutdown(0);
-    });
-    break;
-  case Roles.apihost:
-  default:
-    shutdown(0);
-    break;
-  };
+      break;
+    default:
+      shutdown(0);
+      break;
+  }
 });
 
 process.on("SIGTERM", function() {
@@ -201,26 +227,28 @@ if (!process.env.LOCKER_TEST) {
     try {
       // copy of these in alerting.js so they don't fire alerts too
       var E = err;
-      if(E.toString().indexOf('Error: Parse Error') >= 0)
-      {
+
+      if (E.toString().indexOf('Error: Parse Error') >= 0) {
         // ignoring this for now, relating to some node bug, https://github.com/joyent/node/issues/2997
         logger.warn("ignored exception",E);
         return;
       }
-      if(E.toString().indexOf('ECONNRESET') >= 0 || E.toString().indexOf('socket hang up') >= 0)
-      {
+
+      if (E.toString().indexOf('ECONNRESET') >= 0 || E.toString().indexOf('socket hang up') >= 0) {
         // THEORY: these bubble up from event emitter as uncaught errors, even though the socket end event still fires and are ignorable
         logger.warn("ignored exception",E);
         return;
       }
-      if(E.toString().indexOf('ETIMEDOUT') >= 0)
-      {
+
+      if (E.toString().indexOf('ETIMEDOUT') >= 0) {
         // THEORY: these bubble up from event emitter as uncaught errors, even though the socket end event still fires and are ignorable
         logger.warn("ignored exception",E);
         return;
       }
+
       logger.error('Uncaught exception:');
       logger.error(util.inspect(err));
+
       if (err && err.stack) logger.error(util.inspect(err.stack));
       if (lconfig.airbrakeKey) {
         var airbrake = require('airbrake').createClient(lconfig.airbrakeKey);
@@ -238,10 +266,12 @@ if (!process.env.LOCKER_TEST) {
       } catch (e) {
         // we tried...
       }
+
       process.exit(1);
     }
   });
 }
 
-// Export some things so this can be used by other processes, mainly for the test runner
+// Export some things so this can be used by other processes,
+// mainly for the test runner
 exports.shutdown = shutdown;
