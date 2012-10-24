74278ea52e79602956974361d112856a426388dd
Simon Murtha-Smith <simon@murtha-smith.com>
2012-10-24 11:01:44 -0700

    ensure multi doesn't call multi

--- test/lib/multi.test.js
+++ test/lib/multi.test.js
@@ -6,11 +6,16 @@ var qs = require('querystring');
 
 var api = require('webservice').api;
 var authManager = require('authManager');
+var atok = authManager.provider.generateAccessToken(1, 1, {}).access_token;
 
-var urls = [
-  'http://localhost:8041/v0/services/linkedin/connections?access_token=',
-  'http://localhost:8041/v0/services?access_token='
-]
+var URLS = [
+  'http://localhost:8041/v0/services/linkedin/connections?access_token=' + atok,
+  'http://localhost:8041/v0/services?access_token=' + atok
+];
+
+var MULTI_MULTI = ['/multi', '/v0/multi', 'multi', 'v0/multi'];
+
+var MULTI_SAFE = ['/v0/services/linkedin/multi'];
 
 // TODO: Faking a user through auth with an account and profiles is hard
 describe('Multi Requests', function() {
@@ -18,11 +23,10 @@ describe('Multi Requests', function() {
 
   beforeEach(function(done) {
     require('node-fakeweb').tearDown();
-    access_token = authManager.provider.generateAccessToken(1, 1, {}).access_token;
     done();
   });
 
-  function itValidatesUrls(params) {
+  describe('when you forget the urls parameter', function() {
     it('responds with HTTP 400', function(done) {
       browser(api).
         get('/multi').
@@ -31,49 +35,69 @@ describe('Multi Requests', function() {
           done();
         });
     });
-  }
+  });
 
-  function itAcceptsAFewURLs(urls) {
+  describe('when you include a valid urls parameter', function() {
     it('responds with HTTP 200', function(done) {
-      for(var i in urls) { urls[i] += access_token; }
-      var url = '/multi?urls=' + encodeURIComponent(urls.join(','));
+      var url = '/multi?urls=' + encodeURIComponent(URLS.join(','));
       browser(api).
         get(url).
         expect(200).
         end(function(err, res) {
-          for(var i in urls) {
-            should.exist(res.body[urls[i]]);
+          for(var i in URLS) {
+            should.exist(res.body[URLS[i]]);
           }
           done();
         });
     });
-  }
+  });
 
-  function itDoesntAcceptALotOfUrls() {
+  describe('when you include a urls parameter that is too long', function() {
     it('responds with HTTP 400', function(done) {
       var lots = [];
       for(var i = 0; i < 200; i++) {
-        lots.push(urls[i%2]);
+        lots.push(URLS[i%2]);
       }
-      for(var i in urls) { urls[i] += access_token; }
       var url = '/multi?urls=' + encodeURIComponent(lots.join(','));
       browser(api).
         get(url).
         expect(400).
         end(done);
     });
-  }
-
-  describe('when you forget the urls parameter', function() {
-    itValidatesUrls({});
   });
 
-  describe('when you include a valid urls parameter', function() {
-    itAcceptsAFewURLs(urls);
+  describe('when you include a call to the /multi endpoint', function() {
+    it('doesn\'t allow it', function(done) {
+      var url = '/multi?urls=' + encodeURIComponent(MULTI_MULTI.join(','));
+      browser(api).
+        get(url).
+        expect(200).
+        end(function(err, resp) {
+          for (var i in MULTI_MULTI) {
+            should.exist(resp.body[MULTI_MULTI[i]].error);
+            should.not.exist(resp.body[MULTI_MULTI[i]].body);
+          }
+          done();
+        });
+    });
   });
 
-  describe('when you include a urls parameter that is too long', function() {
-    itDoesntAcceptALotOfUrls(urls);
+  // this one isn't great, because in theory, the error could contain the
+  // word error from somewhere else
+  describe('when you call a URL containing the word multi', function() {
+    it('does allow it', function(done) {
+      var url = '/multi?urls=' + encodeURIComponent(MULTI_SAFE.join(','));
+      browser(api).
+        get(url).
+        expect(200).
+        end(function(err, resp) {
+          for (var i in MULTI_SAFE) {
+            var err = resp.body[MULTI_SAFE[i]].error;
+            should.equal(err.toString().indexOf('mulit'), -1);
+          }
+          done();
+        });
+    });
   });
 
 });
