7b268b2aa6f4201aab3f2e667cd3871eebb79004
Simon Murtha-Smith <simon@murtha-smith.com>
2012-10-26 15:21:28 -0700

    clean up medusa, make it call with limit=10000

--- scripts/medusa.js
+++ scripts/medusa.js
@@ -29,31 +29,49 @@ get('profiles', function(j){
           cb1()
         });
       }, function(){
-        var last = {};
-        if(fs.existsSync(path.join(__dirname,'snaps','last.json'))) try {
-          last = JSON.parse(fs.readFileSync(path.join(__dirname,'snaps','last.json')));
-        }catch(E){console.error(E)}
-        var union = {};
-        Object.keys(snap).forEach(function(k){union[k]=true});
-        Object.keys(last).forEach(function(k){union[k]=true});
-        Object.keys(union).sort().forEach(function(k){
+        var last = readSnapSync() || {};
+        var union = unionKeys(snap, last);
+        union.sort().forEach(function(k){
           var change = (snap[k] > last[k]) ? "MORE" : "LESS";
           if(snap[k] === last[k]) change = "SAME";
           console.log(change,snap[k],last[k],k);
         });
         var end = Date.now();
         console.log(calls,"calls in",(end-start)/1000,"seconds, ",(end-start)/calls,"ms per call");
-        fs.writeFileSync(path.join(__dirname,'snaps','last.json'), JSON.stringify(snap));
-        fs.writeFileSync(path.join(__dirname,'snaps','at.'+Date.now()+'.json'), JSON.stringify(snap));
+        writeSnapSync(snap);
       });
     });
   });
 });
 
-function get(u, cb)
-{
+function unionKeys(snap1, snap2) {
+  var union = {};
+  Object.keys(snap1).forEach(function(k){union[k]=true});
+  Object.keys(snap2).forEach(function(k){union[k]=true});
+  return Object.keys(union);
+}
+
+function readSnapSync(date) {
+  var name = date? 'at.' + date + '.json' : 'last.json';
+  name = path.join(__dirname, 'snaps', name);
+  if (fs.existsSync(name)) {
+    try {
+      var tmp = JSON.parse(fs.readFileSync(name));
+      return tmp;
+    } catch(E) {
+    }
+  }
+}
+
+function writeSnapSync(snap) {
+  var dir = path.join(__dirname, 'snaps');
+  fs.writeFileSync(path.join(dir, 'last.json'), JSON.stringify(snap));
+  fs.writeFileSync(path.join(dir, 'at.' + Date.now() + '.json'), JSON.stringify(snap));
+}
+
+function get(u, cb) {
   calls++;
-  request.get({url:base+u+token, json:true}, function(e,r,j){
+  request.get({url:base+u+token+'&limit=10000', json:true}, function(e,r,j){
     if(e) console.error(u,e) || process.exit(1);
     if(r.statusCode != 200) console.error(u,r.statusCode,j) || process.exit(1);
     if(!j) console.error(u,"no data") || process.exit(1);
