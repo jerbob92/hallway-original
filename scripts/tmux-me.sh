74510fa774d4f8a03d167b8fb260b50570a8e3c2
Beau Gunderson <beau@beaugunderson.com>
2012-11-05 13:13:10 -0800

    Don't start a vim/shell from tmux-me

--- scripts/tmux-me.sh
+++ scripts/tmux-me.sh
@@ -16,13 +16,4 @@ tmux send-keys -t hallway:logs.0 "foreman start worker" C-m
 tmux send-keys -t hallway:logs.1 "foreman start apihost" C-m
 tmux send-keys -t hallway:logs.2 "foreman start dawg" C-m
 
-tmux new-window -n edit "vim"
-
-tmux split-window -t hallway:edit -p 20
-
-# XXX: Needed so the commands actually launch
-sleep 1
-
-tmux select-window -t hallway:logs
-
 tmux attach-session -t hallway
