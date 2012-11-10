7270bfc69c2c9a0570907c166a904e3585f3d15c
Simon Murtha-Smith <simon@murtha-smith.com>
2012-11-10 14:11:36 -0800

    minor mobilizing of other dawg pages
    no formatting, just adding buttons and link interception code

--- static/js/utilities.js
+++ static/js/utilities.js
@@ -1,5 +1,23 @@
 isiPhone = window.navigator && window.navigator.userAgent
         && window.navigator.userAgent.indexOf('iPhone') !== -1;
+
+$(document).on('click', 'a', function(event) {
+  if (!isiPhone) return;
+  var href = $(event.target).attr('href');
+  // only loca urls
+  if (href.indexOf('/') !== 0) return;
+  // only pages (they have a back button on them!)
+  if (href.indexOf('.html') === -1) return;
+
+  // Stop the default behavior of the browser, which
+  // is to change the URL of the page.
+  event.preventDefault();
+
+  // Manually change the location of the page to stay in
+  // 'Standalone' mode and change the URL at the same time.
+  location.href = href;
+});
+
 function commas(number) {
    return String(number).replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
       return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
