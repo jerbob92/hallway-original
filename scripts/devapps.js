01abe68ed6ee7d3a74b82e1153d1b119499b4375
Simon Murtha-Smith <simon@murtha-smith.com>
2012-11-18 12:24:19 -0500

    table-ize scripts/devapps.js

--- scripts/devapps.js
+++ scripts/devapps.js
@@ -5,8 +5,7 @@ var auth = process.argv[2];
 var hours = process.argv[3] || 24;
 var host = process.argv[4] || 'https://dawg.singly.com';
 
-if(!auth)
-{
+if(!auth) {
   console.log("node scripts/devapps.js dawguser:dawgpass");
   process.exit(1);
 }
@@ -17,9 +16,12 @@ req.headers = {"Authorization":"Basic " + new Buffer(auth).toString("base64")};
 req.json = true;
 var until = Date.now() - (3600*hours*1000);
 
-request.get({url:host+'/productionappsactive', headers:req.headers, json:true}, function(err, resp, apps){
-  if(!Array.isArray(apps))
-  {
+console.log('<table><tr>');
+console.log('<td>App ID</td><td>Account</td><td>Name</td><td>Social Profile</td>');
+console.log('</tr>');
+request.get({url:host+'/productionappsactive', headers:req.headers, json:true},
+    function(err, resp, apps) {
+  if(!Array.isArray(apps)) {
     console.log("oops",err,apps);
     process.exit(1);
   }
@@ -27,19 +29,32 @@ request.get({url:host+'/productionappsactive', headers:req.headers, json:true}, 
     accounts(app.app, function(acts){
       var alist = Object.keys(acts).slice(0,3);
       async.forEach(alist, function(act, cbAct){
-        request.get({url:host+'/proxy/'+act+'/profile', headers:req.headers, json:true}, function(err, resp, profile){
-          console.log(app.app,act,profile.name,profile.url," ");
+        request.get({url:host+'/proxy/'+act+'/profile', headers:req.headers,
+          json:true}, function(err, resp, profile) {
+          var html = '<tr>';
+          html += '<td>';
+          html += '<a href="https://dawg.singly.com/apps/get?key=';
+          html += app.app;
+          html += '">' + app.app.substring(0, 6) + '</a></td>';
+          html += '<td>';
+          html += '<a href="https://dawg.singly.com/apps/account?id=' + act;
+          html += '">' + act.substring(0, 6) + '</a></td>';
+          html += '<td>' + profile.name + '</td>';
+          html += '<td><a href="' + profile.url + '">';
+          html += profile.handle + '</a></td>';
+          html += '</tr>';
+          console.log(html);
           cbAct();
         });
       },cbApp);
     });
   }, function(){
+    console.log('</table>');
     process.exit(0);
   });
 });
 
-function accounts(app, cb)
-{
+function accounts(app, cb) {
   req.qs.key = app;
   request.get(req, function(err, res, logs){
     var acts = {};
