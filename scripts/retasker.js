898210d173a198226e518534a64fba56b020e1b5
Simon Murtha-Smith <simon@murtha-smith.com>
2012-12-06 11:26:39 -0800

    add garbage collection to retasker (via --gc)

--- scripts/retasker.js
+++ scripts/retasker.js
@@ -1,8 +1,8 @@
 var argv = require('optimist')
-    .default('concurrent', 15)
+    ['default']('concurrent', 15)
     .boolean('force', false)
-    .default('limit', 100)
-    .default('offset', 0).argv;
+    ['default']('limit', 100)
+    ['default']('offset', 0).argv;
 var async = require('async');
 
 var lconfig = require('lconfig');
@@ -11,8 +11,29 @@ var profileManager = require('profileManager');
 var taskman = require('taskman');
 var dal = require('dal');
 var ijod = require('ijod');
+var servezas = require('servezas');
+var taskStore = require('taskStore');
+var acl = require('acl');
 
-function retask(offset, limit, concurrent, force, service, callback) {
+function retask(pids, concurrent, force, callback) {
+  logger.log("doing "+pids.length+"\n");
+  var i = argv.offset;
+  async.forEachLimit(pids, concurrent, function(row, cbLoop) {
+    logger.log(i++, row.id+"\n");
+    profileManager.authGet(row.id, null, function(err, auth) {
+      if(!auth) return cbLoop();
+      taskman.taskUpdate(auth, function(err) {
+        if(err) logger.error([row.id,err,'\n'].join(' '));
+        cbLoop();
+      }, force);
+    });
+  }, function(err) {
+    if(err) logger.error(err.toString());
+    callback();
+  });
+}
+
+function getPids(offset, limit, service, callback) {
   var sql = 'SELECT id FROM Profiles ';
   var binds = [];
   if(service) {
@@ -22,23 +43,27 @@ function retask(offset, limit, concurrent, force, service, callback) {
   binds.push(limit);
   binds.push(offset);
   sql += 'LIMIT ? OFFSET ?';
-  dal.query(sql, binds, function(err, ret) {
-    if(err || !ret) return callback(err);
-    logger.log("doing "+ret.length+"\n");
-    var i = offset;
-    async.forEachLimit(ret, concurrent, function(row, cbLoop) {
-      logger.log(i++, row.id+"\n");
-      profileManager.authGet(row.id, null, function(err, auth) {
-        if(!auth) return cbLoop();
-        taskman.taskUpdate(auth, function(err) {
-          if(err) logger.error([row.id,err,'\n'].join(' '));
-          cbLoop();
-        }, force);
+  dal.query(sql, binds, callback);
+}
+
+function getOldsTaskPids(callback) {
+  var until = Date.now - (24 * 60 * 60 * 1000);
+  var pids = {};
+  async.forEachLimit(servezas.serviceList(), 10, function(service, cbService) {
+    async.forEachLimit(servezas.syncletList(service), 10, function(synclet, cbSynclet) {
+      taskStore.taskRange(service, synclet, {until:until}, function(err, tasks) {
+        if (err) return cbSynclet(err);
+        for (var i in tasks) {
+          var task = tasks[i];
+          pids[task.pid] = {id: task.pid};
+        }
+        cbSynclet();
       });
-    }, function(err) {
-      if(err) logger.error(err.toString());
-      callback();
-    });
+    }, cbService);
+  }, function(err) {
+    var rows = [];
+    for (var i in pids) { rows.push(pids[i]); }
+    return callback(err, rows);
   });
 }
 
@@ -48,16 +73,24 @@ function init(callback) {
   INITED = true;
   ijod.initDB(function(err) {
     if (err) return callback(err);
-    taskman.init(null, null, callback);
+    taskman.init(null, null, function(err) {
+      if (err) return callback(err);
+      acl.init(callback);
+    });
+  });
+}
+
+function doRetask(err, pids) {
+  if (err) return logger.error('err', err);
+  retask(pids, argv.concurrent, argv.force, function(err) {
+    if (err) logger.error('err', err);
+    logger.log('done');
+    process.exit();
   });
 }
 
 init(function(err) {
   if (err) return logger.error('err', err);
-  retask(argv.offset, argv.limit, argv.concurrent, argv.force, argv.service,
-    function(err) {
-    if (err) logger.error('err', err);
-    logger.log('done');
-    process.exit();
-  });
+  if (argv.gc) getOldsTaskPids(doRetask);
+  else getPids(argv.offset, argv.limit, argv.service, doRetask);
 });
