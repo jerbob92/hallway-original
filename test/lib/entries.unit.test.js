7dec0f518ae35d01386f3cccc714bd73c7c31f8b
Beau Gunderson <beau@beaugunderson.com>
2012-12-17 15:42:30 -0800

    Fix isolated entries.unit.test.js

--- test/lib/entries.unit.test.js
+++ test/lib/entries.unit.test.js
@@ -1,30 +1,36 @@
+require('chai').should();
+
+var dMap = require('dMap');
 var entries = require('entries');
-var should = require("should");
 
-describe("Entries", function() {
-  describe("options", function() {
-    it("should do nothing", function(done) {
+before(dMap.startup);
+
+describe('Entries', function () {
+  describe('options', function () {
+    it('should do nothing', function () {
       entries.options({});
-      done();
-    });
-  });
-  describe("filter", function() {
-    it("should not crash", function(done) {
-      entries.filter([], {});
-      done();
-    });
-  });
-  describe("bases", function() {
-    it("should do services", function(done) {
-      entries.bases("/services/twitter/friends", {}, ['42@twitter','69@twitter']).length.should.equal(2);
-      entries.bases("/services/twitter/friends", {services:'42@twitter'}, ['42@twitter','69@twitter']).length.should.equal(1);
-      done();
-    });
-    it("should do types", function(done) {
-      entries.bases("/types/photos", {}, ['42@twitter','69@facebook']).length.should.equal(2);
-      entries.bases("/types/photos", {services:'twitter'}, ['42@twitter','69@facebook']).length.should.equal(1);
-      done();
     });
   });
 
+  describe('filter', function () {
+    it('should not crash', function () {
+      entries.filter([], {});
+    });
+  });
+
+  describe('bases', function () {
+    it('should do services', function () {
+      entries.bases('/services/twitter/friends', {},
+        ['42@twitter', '69@twitter']).length.should.equal(2);
+      entries.bases('/services/twitter/friends', {services: '42@twitter'},
+        ['42@twitter', '69@twitter']).length.should.equal(1);
+    });
+
+    it('should do types', function () {
+      entries.bases('/types/photos', {}, ['42@twitter', '69@facebook'])
+        .length.should.equal(2);
+      entries.bases('/types/photos', {services: 'twitter'},
+        ['42@twitter', '69@facebook']).length.should.equal(1);
+    });
+  });
 });
