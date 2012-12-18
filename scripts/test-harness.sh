3713a95e2ac16cdcf639c29f60573bf9f2aa41bb
Beau Gunderson <beau@beaugunderson.com>
2012-12-18 10:53:56 -0800

    Use node_modules mocha

--- scripts/test-harness.sh
+++ scripts/test-harness.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MOCHA="node_modules/.bin/mocha -R dot"
 GLOBAL_EXIT_CODE=0
 
 interrupt() {
@@ -12,7 +13,7 @@ trap interrupt SIGINT
 for FILE in `find test -name \*.test.js`; do
    echo "Running $FILE"
 
-   env SUPPRESS_LOGS=true NODE_PATH=lib:test/lib mocha -R dot $FILE
+   env SUPPRESS_LOGS=true NODE_PATH=lib:test/lib $MOCHA $FILE
 
    EXIT_CODE=$?
 
