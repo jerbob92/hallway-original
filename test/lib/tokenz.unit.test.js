51c0510a06d8961c55fbbe4526216e92d14a516f
Beau Gunderson <beau@beaugunderson.com>
2012-12-18 18:24:03 -0800

    Add a test suite for tokenz!

--- test/lib/tokenz.unit.test.js
+++ test/lib/tokenz.unit.test.js
@@ -1,19 +1,122 @@
-var tokenz = require('tokenz');
-var lconfig = require("lconfig");
-if (!lconfig.authSecrets) lconfig.authSecrets = {crypt:"foo", sign:"bar"};
+var should = require('chai').should();
 
-/* this needs to be an integration test
-describe("tokenz", function() {
-  before(tokenz.init);
-  describe("token", function() {
-    it("generated", function(done){
-      tokenz.createAccessToken("foo","bar",{}, function(err, token){
-        console.log(token);
-        token.access_token.should.equal('0156df09');
-        done();        
-      })
+var lconfig = require('lconfig');
+
+lconfig.authSecrets.crypt = 'abc';
+lconfig.authSecrets.sign = '123';
+
+lconfig.minConnections = 1;
+lconfig.maxConnections = 1;
+
+var dalFake = require('dal-fake');
+var dal = require('dal');
+
+dalFake.reset();
+
+var PROFILES = [
+  { profile: '123@facebook' },
+  { profile: '123@twitter' }
+];
+
+dalFake.addFake('SELECT true', []);
+dalFake.addFake('SELECT app, secret, apikeys, notes FROM Apps WHERE app = ? ' +
+  'LIMIT 1',  [{}]);
+dalFake.addFake('SELECT profile FROM Accounts WHERE account = ?', PROFILES);
+
+dal.setBackend('fake');
+
+var acl = require('acl');
+var tokenz = require('tokenz');
+
+var GOOD_ACCESS_TOKEN = 'JrY54j9w8SToWDYFDPDykSZrsR4.EKO8Xl-m96c3fbe6b28d2401' +
+  '22e5858622d1afc7096bebdb0133ee03d334a3fa7d4bb6804a76c76a26fa5697852b8dd07a' +
+  'a32fa65766f6a7d27746a5456fe357b8fa3017';
+
+before(acl.init);
+before(tokenz.init);
+
+// XXX: This is a hack. If we don't prime the DAL pool here then if there are
+// exceptions in actual test cases we'll just see a timeout. The root cause is
+// that if there are 0 database connections in the pool one will be created
+// using the pool's create function, which contains a try/catch. That try/catch
+// prevents mocha from seeing the exception.
+before(function (done) {
+  dal.query('SELECT true', [], done);
+});
+
+describe('tokenz', function () {
+  describe('#init', function () {
+    it('should set tokenz.serializer', function () {
+      should.exist(tokenz.serializer);
+    });
+  });
+
+  describe('#createAccessToken', function () {
+    it('should create a valid access token', function (done) {
+      tokenz.createAccessToken('test-account', 'test-app', {},
+        function (err, token) {
+        if (err) {
+          return done(err);
+        }
+
+        should.exist(token);
+
+        should.exist(token.account);
+        should.exist(token.access_token);
+
+        token.access_token.should.be.a('string');
+        token.access_token.length.should.be.below(255);
+
+        var parsed = tokenz.parseAccessToken(token.access_token);
+
+        parsed.account.should.equal('test-account');
+        parsed.app.should.equal('test-app');
+
+        isNaN(parsed.at).should.equal(false);
+
+        done();
+      });
+    });
+  });
+
+  describe('#parseAccessToken', function () {
+    it('should parse a valid access token', function () {
+      var parsed = tokenz.parseAccessToken(GOOD_ACCESS_TOKEN);
+
+      parsed.account.should.equal('test-account');
+      parsed.app.should.equal('test-app');
+
+      should.exist(parsed.at);
+    });
+  });
+
+  describe('#login', function () {
+    it('should return data with a good access token', function (done) {
+      var res = {
+        json: function (json) {
+          throw json;
+        }
+      };
+
+      var req = {
+        param: function (parameter) {
+          var data = {
+            access_token: GOOD_ACCESS_TOKEN
+          };
+
+          return data[parameter];
+        }
+      };
+
+      tokenz.login(req, res, function () {
+        var token = req._authsome;
+
+        token.profiles.should.equal(PROFILES);
+        token.account.should.equal('test-account');
+        token.app.should.equal('test-app');
+
+        done();
+      });
     });
   });
 });
-
-*/
