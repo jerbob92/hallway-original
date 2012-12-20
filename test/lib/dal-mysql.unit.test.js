53a2c6bc108ff844803ba5a4065693645c1be40e
Beau Gunderson <beau@beaugunderson.com>
2012-12-20 01:14:13 -0800

    Add test suite for dal-mysql

---
+++ test/lib/dal-mysql.unit.test.js
@@ -0,0 +1,62 @@
+var should = require('chai').should();
+
+var lconfig = require('lconfig');
+var mysql = require('dal-mysql');
+
+describe('dal-mysql', function () {
+  describe('#create', function () {
+    it('should create and connect a client', function (done) {
+      mysql.create(lconfig.database, function (err, instance) {
+        if (err) return done(err);
+
+        should.exist(instance);
+
+        done();
+      });
+    });
+  });
+
+  describe('#destroy', function () {
+    var client;
+
+    before(function (done) {
+      mysql.create(lconfig.database, function (err, instance) {
+        if (err) return done(err);
+
+        client = instance;
+
+        done();
+      });
+    });
+
+    it('should destroy an instance', function () {
+      mysql.destroy(client);
+    });
+  });
+
+  describe('Db', function () {
+    var client;
+
+    before(function (done) {
+      mysql.create(lconfig.database, function (err, instance) {
+        if (err) return done(err);
+
+        client = instance;
+
+        done();
+      });
+    });
+
+    after(function () {
+      mysql.destroy(client);
+    });
+
+    describe('#sqlize', function () {
+      it('should properly prepare a statement', function () {
+        var sqlized = client.sqlize('SELECT * FROM table WHERE abc = ?', ['xyz']);
+
+        sqlized.should.equal('SELECT * FROM table WHERE abc = \'xyz\'');
+      });
+    });
+  });
+});
