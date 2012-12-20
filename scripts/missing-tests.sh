eaa2f5a977e960232d521a97fc0e55dc4a98b088
Beau Gunderson <beau@beaugunderson.com>
2012-12-19 17:41:03 -0800

    Also warn on empty test files

--- scripts/missing-tests.sh
+++ scripts/missing-tests.sh
@@ -15,12 +15,14 @@ MISSING=0
 
 RED="\e[1;31m"
 GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
 NOTHING="\e[0m"
 
 X="$RED✘$NOTHING"
 CHECK="$GREEN✔$NOTHING"
+DASH="$YELLOW-$NOTHING"
 
-echo "Missing test files:"
+echo "Missing files and files missing tests:"
 echo
 
 while read -r FILE
@@ -39,6 +41,21 @@ do
   then
     echo -e " $X $UNIT_TEST"
     MISSING=1
+  else
+    HAS_TESTS=0
+
+    grep "\bit(" $TEST 2> /dev/null | grep -v ");" > /dev/null
+
+    HAS_TESTS=$(($HAS_TESTS || $? == 0))
+
+    grep "\bit(" $UNIT_TEST 2> /dev/null | grep -v ");" > /dev/null
+
+    HAS_TESTS=$(($HAS_TESTS || $? == 0))
+
+    if [[ $HAS_TESTS -eq 0 ]]
+    then
+      echo -e " $DASH $UNIT_TEST"
+    fi
   fi
 done < <(find lib -maxdepth 1 -type f)
 
