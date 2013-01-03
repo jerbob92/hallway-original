0366f1735370fe60624d10570db3011f2bbdd4c1
Beau Gunderson <beau@beaugunderson.com>
2013-01-02 18:21:51 -0800

    Add tests for lconfig

--- test/lib/lconfig.unit.test.js
+++ test/lib/lconfig.unit.test.js
@@ -1,1 +1,17 @@
+require('chai').should();
+
 var lconfig = require('lconfig');
+
+describe('lconfig', function () {
+  it('should load configuration data from defaults.json', function () {
+    lconfig.lockerHost.should.equal('localhost');
+    lconfig.lockerPort.should.equal(8042);
+
+    lconfig.externalHost.should.equal('localhost');
+    lconfig.externalPort.should.equal(8042);
+  });
+
+  it('should set externalBase', function () {
+    lconfig.externalBase.should.equal('http://localhost:8042');
+  });
+});
