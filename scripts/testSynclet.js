ec915de8c07f5a8b3c241902a715b6152fc5e538
Beau Gunderson <beau@beaugunderson.com>
2013-01-09 00:53:47 -0800

    Fix broken testSyclet, add pagination suport

--- scripts/testSynclet.js
+++ scripts/testSynclet.js
@@ -1,58 +1,117 @@
-if (process.argv.length != 4) {
-  console.error("testSynclet.js <profile@toRun> <synclet>");
+if (process.argv.length !== 4) {
+  console.error('testSynclet.js <profile@service> <synclet>');
+
   process.exit(1);
 }
 
-var lconfig = require("lconfig");
+var path = require('path');
 
-lconfig.load("Config/config.json");
-
-var dal = require("dal");
-var fs = require("fs");
-var idr = require("idr");
-var logger = require("logger").logger("testSynclet");
-var profileManager = require("profileManager");
-var path = require("path");
-var ijod = require("ijod");
+var async = require('async');
+var dal = require('dal');
+var logger = require('logger').logger('testSynclet');
+var profileManager = require('profileManager');
+var ijod = require('ijod');
 
 var profile = process.argv[2];
 var synclet = process.argv[3];
 
-var service = profile.split("@")[1];
+var service = profile.split('@')[1];
 
-
-logger.info("Running %s/%s for %s", service, synclet, profile);
+logger.info('Running %s/%s for %s', service, synclet, profile);
 
 function exitWithError() {
-  logger.error.apply(logger, arguments);
+  logger.info.apply(logger, arguments);
+
   process.exit(1);
 }
 
-function runService() {
-  dal.query("SELECT service FROM Profiles WHERE id=?", [profile], function(error, rows) {
-    if (error) exitWithError("Error finding the profile %s: %s", profile, error);
-    if (rows.length != 1 || rows[0].service != service) exitWithError("Did not find a valid profile for %s", service);
+var runs = 0;
 
-    profileManager.allGet(profile, function(error, pi) {
-      if (error) exitWithError("Error getting the profile information for %s: %s", profile, error);
-      if (!pi.auth) exitWithError("No auth information was found for the profile %s, you must auth before you can run the synclet.", profile);
+function runService(paginationPi, cb) {
+  dal.query('SELECT service FROM Profiles WHERE id=?', [profile],
+    function (error, rows) {
+    if (error) {
+      exitWithError('Error finding the profile %s: %s', profile, error);
+    }
 
-      //logger.debug("%j", pi);
+    if (rows.length !== 1 || rows[0].service !== service) {
+      exitWithError('Did not find a valid profile for %s', service);
+    }
+
+    profileManager.allGet(profile, function (error, pi) {
+      if (error) {
+        exitWithError('Error getting profile information for %s: %s',
+          profile, error);
+      }
+
+      if (!pi.auth) {
+        exitWithError('No auth information was found for the profile %s,' +
+          ' you must auth before you can run the synclet.', profile);
+      }
+
+      // TODO: Refactor this hack
+      if (paginationPi) {
+        pi = paginationPi;
+        pi.config.nextRun = 0;
+      }
+
       try {
-        var mod = require(path.join(__dirname, "lib", "services", service, synclet) + ".js");
-        if (!mod) exitWithError("Could not find the synclet for %s/%s", service, synclet);
-        mod.sync(pi, function(error, data) {
-          if (error) exitWithError("%s/%s returned the error: %s", service, synclet, error);
-          logger.info("%s/%s returned: %j", service, synclet, data);
-          process.exit(0);
+        var mod = require(path.join(__dirname, '/../lib', 'services', service,
+          synclet) + '.js');
+
+        if (!mod) {
+          exitWithError('Could not find the synclet for %s/%s', service,
+            synclet);
+        }
+
+        mod.sync(pi, function (error, data) {
+          if (error) {
+            exitWithError('%s/%s returned error: %s', service, synclet, error);
+          }
+
+          // TODO: Check for verbose flag
+          var returned = Object.keys(data.data);
+
+          returned = returned.map(function (key) {
+            var result = {};
+
+            if (Array.isArray(data.data[key])) {
+              result[key] = data.data[key].length;
+            } else {
+              result[key] = data.data[key];
+            }
+
+            return result;
+          });
+
+          logger.info('%d %s/%s returned: %s', runs, service, synclet,
+            JSON.stringify(returned));
+
+          cb(data);
         });
-      } catch(E) {
-        exitWithError("Got an exception running %s/%s: %s", service, synclet, E);
+      } catch (e) {
+        exitWithError('Exception running %s/%s: %s', service, synclet, e);
       }
     });
   });
 }
 
-ijod.initDB(function(error) {
-  runService();
+ijod.initDB(function () {
+  var queue = [null];
+
+  async.whilst(function () {
+    return queue.length > 0;
+  }, function (whilstCb) {
+    runs++;
+
+    runService(queue.pop(), function (data) {
+      if (data.config.nextRun === -1) {
+        queue.push(data);
+      }
+
+      whilstCb();
+    });
+  }, function () {
+    process.exit(0);
+  });
 });
