693d0240b3510dc8ca9590fb2af72ca69299394a
Beau Gunderson <beau@beaugunderson.com>
2013-01-21 15:00:31 -0800

    Lint test/lib/taskStore

--- test/lib/taskStore.unit.test.js
+++ test/lib/taskStore.unit.test.js
@@ -1,12 +1,18 @@
+var servezas = require('servezas');
 var taskStore = require('taskStore');
 
-describe('taskStore', function() {
-  describe('getTasks()', function() {
-    it('should not crash', function(done){
-      taskStore.getTasks('s611642@rdio', function(err, tasks) {
-        done()
+before(function () {
+  servezas.load();
+});
+
+describe('taskStore', function () {
+  describe('#getTasks()', function () {
+    it('should not crash', function (done) {
+      taskStore.getTasks('s611642@rdio', function () {
+        // TODO: Actually check that there's no err returned (this will
+        // currently fail because there's no auth info)
+        done();
       });
     });
   });
 });
-
