3f3d9e06713bf0acd7ae187dcd033a22a04f311f
Kristján Pétursson <kristjan@gmail.com>
2013-01-28 16:39:30 -0800

    Add a script to clear all data from a profile
    When we're testing auth or otherwise clean-slate synclet runs, the current flow
    is to wipe out everything in Accounts, Profiles and Entries. Then, you have to
    go log back in to kick everything off again, your access token changes, and
    worst of all, you had to touch the mouse.
    
    This script blows away all IJOD entries, tasks and config data associated with a
    profile, without deleting the profile itself (and its auth data). Go
    nuts...locally.

---
+++ scripts/clearProfile.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env/node
+
+var async = require('async');
+var program = require('commander');
+
+program
+  .usage('-p, --profile <profile@service>')
+  .option('-p, --profile <profile@service>', 'the profile to clear data from')
+  .option('-v, --verbose', 'lots of logging')
+  .parse(process.argv);
+
+
+var lconfig = require('lconfig');
+lconfig.logging = {
+  level: (program.verbose ? 'verbose' : 'info')
+};
+var logger = require('logger').logger('clearProfile');
+
+var dMap = require('dmap');
+var entries = require('entries');
+var ijod = require('ijod');
+var locksmith = require('locksmith');
+var profileManager = require('profileManager');
+var servezas = require('servezas');
+var taskStore = require('taskStore');
+
+
+if (!program.profile) program.help();
+
+var service = program.profile.split('@')[1];
+
+function initialize(callback) {
+  ijod.initDB(function() {
+    locksmith.init('clearProfile', function() {
+      servezas.load();
+      dMap.load();
+      callback();
+    });
+  });
+}
+
+
+function clearBase(base, callback) {
+  ijod.getBounds(base, entries.options({}), function(err, bounds) {
+    if (err) return callback(err);
+
+    logger.info('Clearing', bounds.total, 'entries from', base);
+
+    var toDelete = [];
+
+    ijod.getRange(base, {}, function(item) {
+      toDelete.push(item.idr);
+    }, function(err) {
+      if (err) return callback(err);
+
+      async.forEachLimit(toDelete, 10, ijod.delOne, function(err) {
+        logger.info('Done clearing', base);
+        callback(err);
+      });
+    });
+  });
+}
+
+function resetConfig(callback) {
+  logger.info('Clearing config data');
+  profileManager.reset(program.profile, callback);
+}
+
+function deleteTasks(callback) {
+  logger.info('Deleting all tasks');
+  taskStore.detask(program.profile, callback);
+}
+
+function error(err) {
+  logger.error(err);
+  process.exit(1);
+}
+
+initialize(function() {
+  var bases = dMap.bases([program.profile]);
+
+  logger.info(bases);
+  async.forEach(bases, clearBase, function(err) {
+    if (err) error(err);
+
+    async.parallel([
+      async.apply(resetConfig),
+      async.apply(deleteTasks)
+    ], function(err) {
+      if (err) error(err);
+
+      logger.info('Done');
+      process.exit(0);
+    });
+  });
+});
