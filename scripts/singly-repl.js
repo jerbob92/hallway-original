fe4ee987da91792b0bf159906545a33c4bbd8baa
Beau Gunderson <beau@beaugunderson.com>
2013-02-11 11:02:47 -0800

    Add a REPL that autoloads all Singly modules

---
+++ scripts/singly-repl.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+var _ = require('underscore');
+
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+
+var LIB_PATH = 'lib';
+
+var context = {};
+
+console.log('Loading Singly modules...');
+
+fs.readdir(LIB_PATH, function (err, files) {
+  files.filter(function (file) {
+    return (/\.js$/).test(file);
+  }).forEach(function (file) {
+    context[_.camelize(file.replace('.js', ''))] = require(file);
+  });
+
+  console.log('Finished loading Singly modules.');
+
+  var repl = require('repl').start({});
+
+  _.extend(repl.context, context);
+});
