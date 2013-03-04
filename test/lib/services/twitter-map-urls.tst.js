85507f30de044545f60163d0083538b22113ccef
Thomas Muldowney <temas@sparecog.com>
2013-03-04 15:48:36 -0700

    Name tests and split them

--- test/lib/services/twitter-map-urls.tst.js
+++ test/lib/services/twitter-map-urls.tst.js
@@ -4,28 +4,32 @@ var map = require("services/twitter/map");
 
 var urls = [
   {
+    name: "parentheticals and trailing parens",
     text: "GNOME Discusses Becoming a Linux-only Project http://bit.ly/jBrrAe (http://bit.ly/jO9Pfy) http://bit.ly/j09Pfty) #guru",
     results: [ 'http://bit.ly/jBrrAe', 'http://bit.ly/jO9Pfy', 'http://bit.ly/j09Pfty)' ]
   },
   {
+    name: "exclamation points",
     text: "http://t.co/5D4M9c03.MUCH!!!!!!!!!!!!!!!!!!!!!!!!! \u2665\u2665\u2665",
     results:["http://t.co/5D4M9c03.MUCH!!!!!!!!!!!!!!!!!!!!!!!!!"]
   },
   {
+    name: "odd parens and unicode",
     text: "contrived example: http://somethign.com http://more.com/6o9AEKAk”(",
     results: [ 'http://somethign.com/', 'http://more.com/6o9AEKAk”(' ]
   },
   {
-    text: "Some text with a http://link.to................... with periods after",
-    results: [ 'http://link.to.................../' ]
+    name: "tons of periods",
+    text: "Some text with a http://link.to.............................................................. with periods after",
+    results: [ 'http://link.to............................................................../' ]
   }
 ];
 
 var data = {id_str:"test", user:{id:1}};
 
 describe("twitter url regex", function() {
-  it("handles known troublesome urls", function(done) {
-    urls.forEach(function(urlInfo) {
+  urls.forEach(function(urlInfo) {
+    it("handles known troublesome urls with " + urlInfo.name, function() {
       data.text = urlInfo.text;
       var results = map.tweet.urls(data);
       results.forEach(function(res) {
@@ -35,6 +39,5 @@ describe("twitter url regex", function() {
       });
       expect(urlInfo.results).to.be.empty;
     });
-    done()
   });
 });
